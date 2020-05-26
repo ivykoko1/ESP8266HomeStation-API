0b608bf1ac047774129e007959c03b8c33f306aa
ivykoko1 <ramon.sanchez68@gmail.com>
2020-05-26 09:28:16 +0200

    Nose que he hecho

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-import express from 'express';
-import mysql from 'mysql';
+import express from "express";
+import  urlencoded from "body-parser";
+import moment from "moment"
 
-const api = express();
+
+let app = express();
+
+app.use(urlencoded({ extended: false }));
 
 
-api.post('/api/insertTemperature', async (req, res) => {
-    let paramsOk = req.body.sensorTime && req.body.temperature && req.body.humidity && req.body.temp_index;
-    if(paramsOk){
-        insertSensorData(req.body.sensorTime, req.body.temperature, req.body.humidity, req.body.temp_index);
-        res.status(200).send({"success": true});
-        return
-    }
-})
 
-api.listen(2100, () => {
+app.listen(2100, () => {
     console.log('server running on port 2100');
   });
+  
+  app.use(urlencoded({ extended: false }));
 
-async function insertSensorData(sensorTime, temperature, humidity, temp_index){
-    return insert("sensorTime, temperature, humidity, temp_index", `'${sensorTime}', ${temperature}, ${humidity}, ${temp_index}`, 'sensor_data');
+app.post('/temp', (req, res) => {
+    console.log( req.body.temp);
+    console.log(insertSensorData(0, req.body.temp, 0, 0));
+    /*if(paramsOk){
+        insertSensorData(req.body.sensorTime, req.body.temperature, req.body.humidity, req.body.temp_index);
+
+        return
+    }*/
+            res.status(200).send({"success": true});
+            return;
+})
+
+function insertSensorData(sensorTime, temperature, humidity, temp_index){
+    return insert("sensorTime, temperature, humidity, temp_index", `'${moment().unix()}', ${temperature}, 0, ${temperature}`, 'sensor_data');
 }
 
-async function insert(cols, values, table){
+function insert(cols, values, table){
     let sql = `INSERT INTO ${table}(${cols} VALUES(${values}))`;
     console.log(sql);
 }
