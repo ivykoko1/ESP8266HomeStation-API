c8a81c1be2a78cc566aa0eced9c68bb3bad4203f
ivykoko1 <ramon.sanchez68@gmail.com>
2020-05-28 23:57:59 +0200

    Removed unnecesary code.

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 import express from "express";
 import  urlencoded from "body-parser";
 import moment from "moment"
-
+import mysql  from "mysql"
+import util  from "util"
 
 let app = express();
 
 app.use(urlencoded({ extended: false }));
 
 
+var config = {        
+    host: "localhost",
+    user: "nodejs",
+    password: "nodejs",
+    database: "sensorstation_testing"
+  };
+
+  function makeDb( config ) {
+    const connection = mysql.createConnection( config );
+    return {
+      query( sql, args ) {
+        return util.promisify( connection.query )
+          .call( connection, sql, args );
+      },
+      close() {
+        return util.promisify( connection.end ).call( connection );
+      }
+    };
+  }
 
 app.listen(2100, () => {
     console.log('server running on port 2100');
@@ -16,22 +36,24 @@ app.listen(2100, () => {
   app.use(urlencoded({ extended: false }));
 
 app.post('/temp', (req, res) => {
-    console.log( req.body.temp);
-    console.log(insertSensorData(0, req.body.temp, 0, 0));
-    /*if(paramsOk){
-        insertSensorData(req.body.sensorTime, req.body.temperature, req.body.humidity, req.body.temp_index);
-
-        return
-    }*/
-            res.status(200).send({"success": true});
-            return;
+    let temperature = req.body.temp
+    var ip_address = req.ip.replace('::ffff:', '')
+    console.log(ip_address)
+    insertSensorData(0, temperature, ip_address , 0)
+    res.status(200).send({"success": true});
+    return;
 })
 
-function insertSensorData(sensorTime, temperature, humidity, temp_index){
-    return insert("sensorTime, temperature, humidity, temp_index", `'${moment().unix()}', ${temperature}, 0, ${temperature}`, 'sensor_data');
+function insertSensorData(sensorTime, temperature, ip_address, server_time){
+    return insert("sensorTime, temperature, ip_address", `'${moment().unix()}', ${temperature}, '${ip_address}'`, 'sensor_data');
 }
 
 function insert(cols, values, table){
-    let sql = `INSERT INTO ${table}(${cols} VALUES(${values}))`;
+    let db = makeDb(config);
+    let sql = `INSERT INTO ${table}(${cols}) VALUES(${values});`;
     console.log(sql);
+    db.query(sql);
+    db.close()
+    return true
+
 }
