1b760cd6aa4fd32e9875214ea3f1747af7711264
ivykoko1 <ramon.sanchez68@gmail.com>
2020-06-01 09:11:19 +0200

    backup

--- routes/avg.js
+++ routes/avg.js
@@ -3,6 +3,9 @@ import {raw} from "../db.js"
 export let avgEndpoint = "/avg/:minutes";
 
 export async function getAvg(req, res) {
+    if(req.ip.substring(0, 2) !== '192') {
+        res.status(401).send({"error" : "unauthorized"});
+    }
 	let mins = req.params.minutes;
 	console.log(mins);
     let sql = `select avg(temperature) as avg from sensor_data where (sensorTime + ${mins * 60}) > unix_timestamp();`;
